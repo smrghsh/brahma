5c3760734a3be51efd4008ce9b241a80c7b447d6
Samir Ghosh <hello@samir.tech>
2025-12-11 14:16:23 -0800

    update Selectable with defaults

--- interface-elements/Selectable.js
+++ interface-elements/Selectable.js
@@ -3,8 +3,8 @@ import Experience from "../../Experience.js";
 
 export default class Selectable extends THREE.Mesh {
   constructor(
-    geometry,
-    material,
+    geometry = new THREE.BoxGeometry(1, 1, 1),
+    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
     name = "not named",
     initialColor = 0x00ff00,
     hoveredColor = 0xff0000
@@ -21,14 +21,16 @@ export default class Selectable extends THREE.Mesh {
   onHover() {
     this.hover = true;
     this.material.color.set(this.hoveredColor);
+    this.material.needsUpdate = true;
     console.log(this.name + " hovered");
   }
   onUnhover() {
     this.hover = false;
     this.material.color.set(this.initialColor);
+    this.material.needsUpdate = true;
     console.log(this.name + " exited");
   }
   onSelect() {
-    console.log(this.name + " triggered");
+    console.log(this.name + " selected");
   }
 }
